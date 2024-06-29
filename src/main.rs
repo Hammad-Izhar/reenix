c86aabbd8f952946b1b8e3c75fc6a62cad84adc5
Hammad Izhar <hammad_izhar@brown.edu>
2024-06-29 00:01:04 -0400

    feat: some logging infrastructure

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,24 @@ mod util;
 
 use core::{arch::global_asm, panic::PanicInfo};
 
+use util::debug::DebugMode;
+
 global_asm!(include_str!("boot/boot.S"));
 
 #[no_mangle]
 pub extern "C" fn kmain() {
     util::debug::dbg_init();
 
+    dbg_println!(DebugMode::Error);
+    dbg_println!(DebugMode::Error, "Hello, World!");
+    dbg_println!(DebugMode::Error, "{}, World!", "Hello");
+
     loop {}
 }
 
 #[panic_handler]
-fn panic_handler(_info: &PanicInfo) -> ! {
+fn panic_handler(info: &PanicInfo) -> ! {
+    dbg_println!(DebugMode::Error, "{}", info);
+
     loop {}
 }
--- src/util/debug.rs
+++ src/util/debug.rs
@@ -1,3 +1,5 @@
+use yansi::{Color, Style};
+
 use crate::drivers::uart16550::SerialDriver;
 
 // TODO: Make Synchronization Safe
@@ -5,10 +7,21 @@ use crate::drivers::uart16550::SerialDriver;
 /// QEMU COM1 serial port is at 0x3F8 which we'll use as a debug port.
 pub static mut COM1: SerialDriver = unsafe { SerialDriver::new(0x3F8) };
 
-#[derive(Debug)]
+pub static LOG_LEVEL: DebugMode = DebugMode::Trace;
+pub static ERROR: Style = Color::Red.on_primary();
+pub static WARN: Style = Color::Yellow.on_primary();
+pub static INFO: Style = Color::Green.on_primary();
+pub static DEBUG: Style = Color::Cyan.on_primary();
+pub static TRACE: Style = Color::Magenta.on_primary();
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 #[allow(dead_code)]
 pub enum DebugMode {
-    Panic,
+    Trace = 0,
+    Info,
+    Debug,
+    Warn,
+    Error,
 }
 
 pub fn dbg_init() {
@@ -19,24 +32,83 @@ pub fn dbg_init() {
 
 #[macro_export]
 macro_rules! dbg_print {
-    ($typ:expr, $($arg:tt)*) => {{
+    ($($arg:tt)*) => {{
         use core::fmt::Write;
         unsafe {
-            write!(COM1, $($arg)*).unwrap();
+            write!($crate::util::debug::COM1, $($arg)*).unwrap();
         }
     }};
 }
 
 #[macro_export]
 macro_rules! dbg_println {
-    ($typ:expr) => {
-        $crate::dbg_print!($typ, "\n")
-    };
-    ($typ:expr, $fmt: expr) => {
-        $crate::dbg_print!($typ, concat!($fmt, "\n"))
-    };
+    ($typ:expr) => {{
+        use core::panic::Location;
+        use yansi::Paint;
 
-    ($typ:expr, $fmt: expr, $($arg:tt)*) => {
-        $crate::dbg_print!($typ, concat!($fmt, "\n"), $($arg)*)
-    };
+        let location = Location::caller();
+
+        let (dbg_type, dbg_style) = match $typ {
+            $crate::util::debug::DebugMode::Error => ("ERROR", $crate::util::debug::ERROR),
+            $crate::util::debug::DebugMode::Warn => ("WARN", $crate::util::debug::WARN),
+            $crate::util::debug::DebugMode::Info => ("INFO", $crate::util::debug::INFO),
+            $crate::util::debug::DebugMode::Debug => ("DEBUG", $crate::util::debug::DEBUG),
+            $crate::util::debug::DebugMode::Trace => ("TRACE", $crate::util::debug::TRACE),
+        };
+
+        if $typ >= $crate::util::debug::LOG_LEVEL {
+
+        $crate::dbg_print!(
+            "[{} {}:{}]\n",
+            dbg_type.paint(dbg_style),
+            location.file(),
+            location.line()
+        );
+    }
+    }};
+
+    ($typ:expr, $fmt: expr) => {{
+        use core::panic::Location;
+        use yansi::Paint;
+
+        let location = Location::caller();
+
+        let (dbg_type, dbg_style) = match $typ {
+            $crate::util::debug::DebugMode::Error => ("ERROR", $crate::util::debug::ERROR),
+            $crate::util::debug::DebugMode::Warn => ("WARN", $crate::util::debug::WARN),
+            $crate::util::debug::DebugMode::Info => ("INFO", $crate::util::debug::INFO),
+            $crate::util::debug::DebugMode::Debug => ("DEBUG", $crate::util::debug::DEBUG),
+            $crate::util::debug::DebugMode::Trace => ("TRACE", $crate::util::debug::TRACE),
+        };
+
+        $crate::dbg_print!(
+            concat!("[{} {}:{}] ", $fmt, "\n"),
+            dbg_type.paint(dbg_style),
+            location.file(),
+            location.line(),
+        );
+    }};
+
+    ($typ:expr, $fmt: expr, $($arg:tt)*) => {{
+        use core::panic::Location;
+        use yansi::Paint;
+
+        let location = Location::caller();
+
+        let (dbg_type, dbg_style) = match $typ {
+            $crate::util::debug::DebugMode::Error => ("ERROR", $crate::util::debug::ERROR),
+            $crate::util::debug::DebugMode::Warn => ("WARN", $crate::util::debug::WARN),
+            $crate::util::debug::DebugMode::Info => ("INFO", $crate::util::debug::INFO),
+            $crate::util::debug::DebugMode::Debug => ("DEBUG", $crate::util::debug::DEBUG),
+            $crate::util::debug::DebugMode::Trace => ("TRACE", $crate::util::debug::TRACE),
+        };
+
+        $crate::dbg_print!(
+            concat!("[{} {}:{}] ", $fmt, "\n"),
+            dbg_type.paint(dbg_style),
+            location.file(),
+            location.line(),
+            $($arg)*
+        );
+    }};
 }
